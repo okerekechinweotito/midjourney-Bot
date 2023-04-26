9092f9b93445e91997045e8fa791a3d1f3c42191
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-04-26 11:06:25 +0100

    refactor code

---
+++ fetchMidjourney.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+
+const useMidjourney = async () => {
+  const url = 'https://ai_image-database.p.rapidapi.com/images/midjourney';
+  const options = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/octet-stream',
+      'X-RapidAPI-Key': '991c511ab0msh8758e4b8b60f38dp183e5ejsnb33d53be312b',
+      'X-RapidAPI-Host': 'ai_image-database.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    let result = await response.text();
+    result = JSON.parse(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { useMidjourney };
--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 import { TwitterApi } from 'twitter-api-v2';
+import { useMidjourney } from './fetchMidjourney.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const generateNewImage = async () => {
+  const dataPool = await useMidjourney();
+  const randomIndex = Math.floor(Math.random() * dataPool.length);
+  const randomData = await dataPool[randomIndex];
+  console.log(randomData.prompt);
+  return randomData;
+};
+
+generateNewImage();
+
 const handleTweet = () => {
-    const twitterClient = new TwitterApi({
-        appKey: process.env.CONSUMER_KEY ?? '',
-        appSecret: process.env.CONSUMER_SECRET ?? '',
-        accessToken: process.env.ACCESS_TOKEN ?? '',
-        accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
-    });
+  const twitterClient = new TwitterApi({
+    appKey: process.env.CONSUMER_KEY ?? '',
+    appSecret: process.env.CONSUMER_SECRET ?? '',
+    accessToken: process.env.ACCESS_TOKEN ?? '',
+    accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
+  });
+  const tweetClient = twitterClient.readWrite;
 
-    const tweetClient = twitterClient.readWrite;
+  const image = generateNewImage();
 
-    tweetClient.v2.tweet('Bot test');
+  tweetClient.v2.tweet(image.prompt);
 };
 
 handleTweet();
