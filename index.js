693a3524161d49b0e2af11d9f363498b873a0b99
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-05-04 08:18:37 +0100

    add hashtags

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const handleTweet = async () => {
 
   const image = await useFirebase();
   await downloadImage(image.smallImage, './image.png');
-  const prompt = await shortenString(image.prompt, 170);
+  const prompt = await shortenString(image.prompt, 165);
   const upload = await tweetClient.v1.uploadMedia('./image.png');
-  const tweetText = `${prompt} #midjourney #midjourneyv5 #midjourneyart #MidJourneyArtwork #AI #AIArt #AIArtCommunity #AIGenerated`;
+  const tweetText = `${prompt} #midjourney #midjourneyv5 #midjourneyart #MidJourneyArtwork #AI #AIArt #AIArtCommunity #AIGenerated #Bot`;
 
   const resp = await tweetClient.v2.tweet(tweetText, {
     media: {
