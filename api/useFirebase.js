56a443ae5dbe166fe1fe793f8add826776003461
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-05-26 10:08:39 +0100

    avoid duplicate

--- api/useFirebase.js
+++ api/useFirebase.js
@@ -1,17 +1,19 @@
 import { database } from './firebaseConfig.js';
-import { get, ref, child } from 'firebase/database';
+import { get, ref, child, remove } from 'firebase/database';
 
 const fetchImage = async (dataKey) => {
   const databaseRef = ref(database);
   const snapshot = await get(child(databaseRef, dataKey));
   const data = snapshot.val();
+  await remove(child(databaseRef, dataKey));
   return data;
 };
 
 const useFirebase = async () => {
   const uniqueRandom = Date.now() + Math.random();
   const randomIndex = Math.floor(uniqueRandom * 5425) % 5425;
-  const randomData = await fetchImage(randomIndex.toString());
+  // const randomData = await fetchImage(randomIndex.toString());
+  const randomData = await fetchImage('0');
   return randomData;
 };
 
